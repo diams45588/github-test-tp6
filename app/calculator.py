b0185bd0a5a6ca23ad4ac484d544c4c0f2315fb9
diams45588 <diaminatousanogo4@gmail.com>
2025-11-12 22:58:17 +0000

    Qualité du code flake final

--- app/calculator.py
+++ app/calculator.py
@@ -1,32 +1,37 @@
-﻿def add(a,b):
-    '''Additionne deux nombres'''
-    return a+b
+﻿def add(a, b):
+    """Additionne deux nombres"""
+    return a + b
+
 
 def subtract(a, b):
-    result=a-b
+    result = a - b
     return result
 
-def multiply(a,b):
+
+def multiply(a, b):
     return a * b
 
-def divide(a,b):
-    if b==0:
+
+def divide(a, b):
+    if b == 0:
         raise ValueError("Cannot divide by zero")
     else:
-        return a/b
+        return a / b
+
 
 def unused_function():
     # Cette fonction n'est pas utilisée
-    x=1
-    y=2
-    return x+y
+    x = 1
+    y = 2
+    return x + y
+
 
 class Calculator:
     def __init__(self):
-        self.value=0
-    
+        self.value = 0
+
     def increment(self):
-        self.value+=1
-    
+        self.value += 1
+
     def get_value(self):
         return self.value
--- app/utils.py
+++ app/utils.py
@@ -1,14 +1,18 @@
-﻿import os,sys
+﻿import os, sys
 
-def long_function_name_with_many_parameters( param1,  param2, param3, param4, param5, param6):
+
+def long_function_name_with_many_parameters(
+    param1, param2, param3, param4, param5, param6
+):
     # Cette fonction a un nom trop long et trop de paramètres
-    result=param1+param2+param3+param4+param5+param6
+    result = param1 + param2 + param3 + param4 + param5 + param6
     return result
 
+
 def another_function():
-    x=1
-    y=2
-    z=3
-    if x==1 and y==2 and z==3:
+    x = 1
+    y = 2
+    z = 3
+    if x == 1 and y == 2 and z == 3:
         print("hello")
-    return x+y+z
+    return x + y + z
